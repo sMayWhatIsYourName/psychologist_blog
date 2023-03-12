6608dfad69b25d6d6eee22581b6bfa7f9c774afe
sMayWhatIsYourName <guadarramadeveloper@gmail.com>
2023-03-13 04:22:59 +0500

    Add headless CMS (Contentful), routing, remove default files

---
+++ lib/contentful/client.ts
@@ -0,0 +1,6 @@
+const contentful = require('contentful')
+
+const space = process.env.CONTENTFUL_SPACE_ID || '';
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN || '';
+
+export const client = contentful.createClient({ space, accessToken });
