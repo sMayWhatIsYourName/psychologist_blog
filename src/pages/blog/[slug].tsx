7c16ab95dd88f14aad408245ad30b919650db10a
sMayWhatIsYourName <guadarramadeveloper@gmail.com>
2023-03-13 04:23:21 +0500

    Add headless CMS (Contentful), routing, remove default files

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,52 @@
+import { client } from "../../../lib/contentful/client";
+
+export default function BlogItem({ post }) {
+  console.log(post);
+
+  return (
+    <>
+      <main>
+        <div>
+          <h3>{post.fields.title}</h3>
+          <p>{post.fields.description}</p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const response = await client.getEntries({
+    content_type: "post",
+    "fields.slug": slug,
+  });
+
+  if (!response?.items?.length) {
+    return {
+      redirect: {
+        destination: "/blog",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      post: response?.items?.[0],
+      revalidate: 60,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "post" });
+  const paths = res.items.map((item) => ({
+    params: { slug: item.fields.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { client } from "../../../lib/contentful/client";
+
+export default function Blog({ posts }) {
+  return (
+    <>
+      <main>
+        BLOG
+        <ul className="flex-col">
+          {posts.map((post, i) => <Link href={`/blog/${post.fields.slug}`} key={i}>
+            <div>
+              <h3>{post.fields.title}</h3>
+              <p>{post.fields.description}</p>
+            </div>
+          </Link>)}
+        </ul>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await client.getEntries({ content_type: 'post' });
+  console.log(res.items);
+
+  return {
+    props: {
+      posts: res.items,
+      revalidate: 60
+    }
+  }
+};
